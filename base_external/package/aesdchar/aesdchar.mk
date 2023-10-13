dfdae7d8705a731f59b507c62d53b2098c07d233
pvskpraveen <pvskpraveen@yahoo.com>
2023-10-13 17:53:24 -0600

    assignment8

---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,22 @@
+
+##############################################################
+#
+# AESDCHAR
+#
+##############################################################
+
+AESDCHAR_VERSION = d94ce021f892c4b781b856392da2900ca4f155fc	
+AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-pvskpraveen-1.git
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
